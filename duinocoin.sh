0f80dbbcc93288bc28fffc47384cc58daf603657
yosefhuang1 <yosefhuang1@gmail.com>
2021-08-26 11:51:47 +0700

    added API duco price

--- duinocoin.sh
+++ duinocoin.sh
@@ -8,12 +8,14 @@ MSG="ᕲ DuinoCoin"
 # PWB="Powered by azagramac"
 JSON=$(curl -s -X GET https://server.duinocoin.com/users/$WALLET -H "Accept: application/json" | jq .)
 #EURO=$(curl -s -X GET http://www.floatrates.com/daily/usd.json -H "Accept: application/json" | jq .eur | jq '.rate')
+APIJSON=$(curl -s -X GET https://server.duinocoin.com/api.json -H "Accept: application/json" | jq .)
 USR=$(echo $JSON | jq '.result.balance.username')
 BALANCE=$(echo $JSON | jq '.result.balance.balance')
 WORKERS=$(echo $JSON | jq '.result.miners' | jq '.[].identifier')
 NUMBER_WORKERS=$(echo $JSON | jq -r '.result.miners' | jq -r '.[].identifier' | wc -l)
 FORMAT1=$(echo "scale=2; $BALANCE/1" | bc -l)
 #FORMAT2=$(echo "scale=2; $EURO/1" | bc -l)
+DUCOPRICE=$(echo $APIJSON | jq '."Duco price"')
 
 if [ $? -ne 0 ]
 then
@@ -22,7 +24,7 @@ else
         /usr/bin/curl -s -X POST $URL \
                 -d chat_id=$ID \
                 -d parse_mode=HTML \
-                -d text="$(printf "$MSG\n\t \U1F4B3 ID: <code>$USR</code>\n\t \U1FA99 Balance: <code>$FORMAT1 ᕲ</code>\n\t  \U26CF Nº Workers $NUMBER_WORKERS:\n<code>$WORKERS</code>")" \
+                -d text="$(printf "$MSG\n\t \U1F4B3 ID: <code>$USR</code>\n\t \U1FA99 Balance: <code>$FORMAT1 ᕲ</code>\n\t \U0024 Duco Price: <code>$DUCOPRICE ᕲ</code>\n\t \U26CF Nº Workers $NUMBER_WORKERS:\n<code>$WORKERS</code>")" \
                 > /dev/null 2>&1
         exit 0
 fi

14b7721678684179b423ca84b3867ec0e3c0ec72
yosefhuang1 <yosefhuang1@gmail.com>
2021-09-02 10:29:29 +0700

    added verify check

--- duinocoin.sh
+++ duinocoin.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-TOKEN="YOUR_TOKEN"
-ID="ID_CHAT"
-WALLET="USERNAME_WALLET"
+TOKEN="1946760113:AAGXvjFKOh9u427OSIpzVzLRFl-6RNUqJTA"
+ID="556989570"
+WALLET="yosefhuang1"
 URL="https://api.telegram.org/bot$TOKEN/sendMessage"
 MSG="ᕲ DuinoCoin"
 # PWB="Powered by azagramac"
@@ -10,6 +10,7 @@ JSON=$(curl -s -X GET https://server.duinocoin.com/users/$WALLET -H "Accept: app
 #EURO=$(curl -s -X GET http://www.floatrates.com/daily/usd.json -H "Accept: application/json" | jq .eur | jq '.rate')
 APIJSON=$(curl -s -X GET https://server.duinocoin.com/api.json -H "Accept: application/json" | jq .)
 USR=$(echo $JSON | jq '.result.balance.username')
+VERIFYCHECK=$(echo $JSON | jq '.result.balance.verified')
 BALANCE=$(echo $JSON | jq '.result.balance.balance')
 WORKERS=$(echo $JSON | jq '.result.miners' | jq '.[].identifier')
 NUMBER_WORKERS=$(echo $JSON | jq -r '.result.miners' | jq -r '.[].identifier' | wc -l)
@@ -24,7 +25,7 @@ else
         /usr/bin/curl -s -X POST $URL \
                 -d chat_id=$ID \
                 -d parse_mode=HTML \
-                -d text="$(printf "$MSG\n\t \U1F4B3 ID: <code>$USR</code>\n\t \U1FA99 Balance: <code>$FORMAT1 ᕲ</code>\n\t \U0024 Duco Price: <code>$DUCOPRICE ᕲ</code>\n\t \U26CF Nº Workers $NUMBER_WORKERS:\n<code>$WORKERS</code>")" \
+                -d text="$(printf "$MSG\n\t \U1F4B3 ID: <code>$USR</code>\n\t \U2705 Verify: <code>$VERIFYCHECK</code>\n\t \U1FA99 Balance: <code>$FORMAT1 ᕲ</code>\n\t \U0024 Duco Price: <code>$DUCOPRICE ᕲ</code>\n\t \U26CF Nº Workers $NUMBER_WORKERS:\n<code>$WORKERS</code>")" \
                 > /dev/null 2>&1
         exit 0
 fi
